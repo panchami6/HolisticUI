4479700558c65a791220852fce7dfe8fdd40dc3d
panchami6 <62180655+panchami6@users.noreply.github.com>
2021-03-28 02:08:20 +0530

    component library

---
+++ app.js
@@ -0,0 +1,23 @@
+let modalBtn = document.querySelector('.modal-btn');
+let modalBg = document.querySelector('.modal-bg');
+let modalClose = document.querySelector('.modal-close');
+let snackBarBtn = document.querySelector('.snack-btn');
+let snackBar = document.querySelector('.snackBar');
+let snackbarClose = document.querySelector('.snackbar-close');
+
+modalBtn.addEventListener("click", function(){
+    modalBg.classList.add('bg-active');
+});
+
+modalClose.addEventListener("click", function(){
+    modalBg.classList.remove('bg-active');
+})
+
+snackBarBtn.addEventListener("click", function(){
+    snackBar.classList.add('snack-active');
+});
+
+snackbarClose.addEventListener("click", function(){
+    snackBar.classList.remove('snack-active');
+})
+
